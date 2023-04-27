81a551c408daca07fbc2d92495547ac222fbe501
k022g0059@g.neec.ac.jp <k022g0059@g.neec.ac.jp>
2023-04-27 13:36:30 +0900

    00_05_確認課題

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <Novice.h>
 #include "Matrix4x4.h"
 #include "Vector3.h"
+#include <cmath>
 
 const char kWindowTitle[] = "LE1A_16_マキユキノリ_タイトル";
 
@@ -10,11 +11,35 @@ static const int kColumnWidth = 60;
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* name);
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
 
+// 行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+// 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+// 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+// X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian);
+// Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian);
+// Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian);
+
+// アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate);
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Vector3 scale{ 1.2f, 0.79f, -2.1f };
+	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
+	Vector3 translate{ 2.7f, -4.15f, 1.57f };
+
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -32,7 +57,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-
+		Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 		///
 		/// ↑更新処理ここまで
 		///
@@ -40,7 +65,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
@@ -73,4 +98,151 @@ void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) 
 	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
 	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
 	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+// 行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result;
+	for (int line = 0; line < 4; line++) {
+		for (int j = 0; j < 4; j++) {
+			result.m[line][j] = 0;
+		}
+	}
+	for (int line = 0; line < 4; line++) {
+		for (int column = 0; column < 4; column++) {
+			result.m[line][column] =
+				m1.m[line][0] * m2.m[0][column] + m1.m[line][1] * m2.m[1][column] +
+				m1.m[line][2] * m2.m[2][column] + m1.m[line][3] * m2.m[3][column];
+		}
+	}
+	return result;
+}
+
+// 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	Matrix4x4 result;
+	for (int line = 0; line < 4; line++) {
+		for (int column = 0; column < 4; column++) {
+			if (line == column) {
+				result.m[line][column] = 1;
+			}
+			else {
+				result.m[line][column] = 0;
+			}
+		}
+	}
+
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+
+	return result;
+}
+
+// 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result;
+	for (int line = 0; line < 4; line++) {
+		for (int column = 0; column < 4; column++) {
+			if (line == column) {
+				result.m[line][column] = 1;
+			}
+			else {
+				result.m[line][column] = 0;
+			}
+		}
+	}
+
+	result.m[0][0] = scale.x;
+	result.m[1][1] = scale.y;
+	result.m[2][2] = scale.z;
+
+	return result;
+}
+
+// X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 result;
+	for (int line = 0; line < 4; line++) {
+		for (int column = 0; column < 4; column++) {
+			if (line == column) {
+				result.m[line][column] = 1;
+			}
+			else {
+				result.m[line][column] = 0;
+			}
+		}
+	}
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
+	result.m[2][1] = -std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	return result;
+}
+
+// Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 result;
+	for (int line = 0; line < 4; line++) {
+		for (int column = 0; column < 4; column++) {
+			if (line == column) {
+				result.m[line][column] = 1;
+			}
+			else {
+				result.m[line][column] = 0;
+			}
+		}
+	}
+	result.m[0][0] = std::cos(radian);
+	result.m[0][2] = -std::sin(radian);
+	result.m[2][0] = std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	return result;
+}
+
+// Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 result;
+	for (int line = 0; line < 4; line++) {
+		for (int column = 0; column < 4; column++) {
+			if (line == column) {
+				result.m[line][column] = 1;
+			}
+			else {
+				result.m[line][column] = 0;
+			}
+		}
+	}
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[1][0] = -std::sin(radian);
+	result.m[1][1] = std::cos(radian);
+	return result;
+}
+
+// アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate) {
+	Matrix4x4 result;
+	for (int line = 0; line < 4; line++) {
+		for (int column = 0; column < 4; column++) {
+			result.m[line][column] = 0;
+		}
+	}
+	// スケーリング行列の作成
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+
+	// X軸周り回転行列の作成
+	Matrix4x4 rotateX = MakeRotateXMatrix(rot.x);
+	// Y軸周り回転行列の作成
+	Matrix4x4 rotateY = MakeRotateYMatrix(rot.y);
+	// Z軸周り回転行列の作成
+	Matrix4x4 rotateZ = MakeRotateZMatrix(rot.z);
+	// 回転行列の合成(Z回転 * X回転 * Y回転)
+	Matrix4x4 rotateMatrix = Multiply(rotateX, Multiply(rotateY, rotateZ));
+
+	// 平行移動行列の作成
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+
+	result = Multiply(Multiply(scaleMatrix, rotateMatrix), translateMatrix);
+	return result;
 }
