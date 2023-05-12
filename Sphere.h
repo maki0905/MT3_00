9c818bd19183d2645707703ad7bc0bff17f68850
k022g0059@g.neec.ac.jp <k022g0059@g.neec.ac.jp>
2023-05-12 10:34:25 +0900

    01_02

---
+++ Sphere.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <Vector3.h>
+struct Sphere {
+	Vector3 center; // ’†S“_
+	float radius; // ”¼Œa
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <Novice.h>
 #include "Matrix4x4.h"
 #include "Vector3.h"
+#include "Sphere.h"
+#include <imgui.h>
+#include <cstdint>
 
 const char kWindowTitle[] = "LE1A_16_マキユキノリ_タイトル";
 
@@ -10,11 +13,18 @@ static const int kColumnWidth = 60;
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* name);
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
 
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewporMatrix);
+void DrawShere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// カメラの位置と角度
+	Vector3 cameraTranslate{ 0.0f, 1.9f, -6.49f };
+	Vector3 cameraRotate{ 0.26f, 0.0f, 0.0f };
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -73,4 +83,25 @@ void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) 
 	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
 	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
 	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewporMatrix)
+{
+	const float kGridHalfwidth = 2.0f; // Gridの半分の幅
+	const uint32_t kSubdivision = 10; // 分割数
+	const float kGridEvery = (kGridHalfwidth * 2.0f) / float(kSubdivision); // 1つ分の長さ
+	// 奥から手前への線を順々に引いていく
+	for (uint32_t zIndex = 0; zIndex <= kSubdivision; ++zIndex) {
+		// ワールド座標系上の始点と終点を求める
+		
+		// スクリーン座標系まで変換をかける
+		
+		// 変換した座標を使って表示、色は薄い灰色(0xAAAAAAFF)、原点は黒ぐらいが良いが、なんでも良い
+		
+	}
+}
+
+void DrawShere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
+{
+
 }
